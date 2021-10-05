ed57d5a8dc59d34860579664aad6ba3bb624177b
Levi Schwartzberg <Levischwartzberg@gmail.com>
2021-10-05 12:32:08 -0500

    Added the remaining crud routes as well as get by id and sku

--- src/main/java/com/astontech/rest/ProductRest.java
+++ src/main/java/com/astontech/rest/ProductRest.java
@@ -1,8 +1,6 @@
 package com.astontech.rest;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/product")
@@ -14,8 +12,46 @@ public class ProductRest {
         this.productRepo = productRepo;
     }
 
+    //CRUD Functions
+
     @GetMapping("/")
     public Iterable<Product> getAllProducts() {
         return productRepo.findAll();
+    }
+
+    @PostMapping("/")
+    public Product addProduct(@RequestBody Product product) {
+        return productRepo.save(product);
+    }
+
+    @PutMapping("/")
+    public Product updateProduct(@RequestBody Product product) {
+        return productRepo.save(product);
+    }
+
+    @DeleteMapping("/")
+    public void deleteProduct(@RequestBody Product product) {
+        productRepo.delete(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Integer id) {
+        productRepo.deleteById(id);
+    }
+
+    //Query Params
+    @GetMapping()
+    public Product findByIdOrSku(@RequestParam(required = false) Integer id,
+                                 @RequestParam(required = false) String sku) {
+        if (sku != null && !sku.isEmpty()) {
+            return productRepo.findBySku(sku);
+        }
+        else if (id != null) {
+            return productRepo.findById(id).get();
+        }
+        else {
+            //throw custom exception
+            return new Product();
+        }
     }
 }
